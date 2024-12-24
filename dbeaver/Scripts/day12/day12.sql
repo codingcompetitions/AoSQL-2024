49738bd5e83772de740b4d7348c8ec4c33dd7fa4
Manuel Alejandro Gómez Nicasio <az-dev@outlook.com>
2024-12-24 14:24:01 -0600

    Day 12

--- dbeaver/Scripts/day12/day12.sql
+++ dbeaver/Scripts/day12/day12.sql
@@ -4,6 +4,30 @@
 -- file that was distributed with this source code.
 
 --
+-- PERCENT_RANK
+--      https://www.postgresql.org/docs/current/functions-window.html#FUNCTIONS-WINDOW-TABLE
+--      https://neon.tech/postgresql/postgresql-window-function/postgresql-percent_rank-function
 --
+-- DISTINCT ON
+--      https://www.postgresql.org/docs/current/sql-select.html#:~:text=chosen%20query%20plan.-,DISTINCT%20Clause,-If%20SELECT%20DISTINCT
+--      https://neon.tech/postgresql/postgresql-tutorial/postgresql-distinct-on
 --
+-- What on earth does percent_rank do? I found it reading more about window
+-- functions it just works but I don't know how it works.
 --
+
+WITH GIFT_DATA AS (
+    SELECT DISTINCT
+        G.GIFT_NAME
+        , COUNT(GR.REQUEST_ID) OVER (PARTITION BY G.GIFT_NAME) AS TOTAL_GIFT_REQUESTS
+    FROM
+        GIFTS G
+        JOIN GIFT_REQUESTS GR ON G.GIFT_ID = GR.GIFT_ID
+)
+SELECT DISTINCT ON (OVERALL_RANK)
+    GD.GIFT_NAME
+    , ROUND(PERCENT_RANK() OVER(ORDER BY GD.TOTAL_GIFT_REQUESTS)::NUMERIC, 2) AS OVERALL_RANK
+FROM
+    GIFT_DATA GD
+ORDER BY OVERALL_RANK DESC, GD.GIFT_NAME ASC
+LIMIT 2
